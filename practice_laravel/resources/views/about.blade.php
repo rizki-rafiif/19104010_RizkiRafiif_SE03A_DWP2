79bf015c0bce80d8b5c9f769c1ea9c128b269b26
Rizki Rafiif <58683476+rizki-rafiif@users.noreply.github.com>
2022-01-02 20:54:08 +0700

    upload praktikum 8 practice laravel

---
+++ practice_laravel/resources/views/about.blade.php
@@ -0,0 +1,5 @@
+@extends('templates.base')
+@section('content')
+    <h1>About</h1>
+    <p>Ini adalah praktikum Desain Pemrograman Web 2 yang mempelajari framework PHP yaitu Laravel</p>
+@endsection
